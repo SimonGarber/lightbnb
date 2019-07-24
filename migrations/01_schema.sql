095fcc776dab3692b3ec46730bda993d3c92d16e
Simon Garber <Simon.garber@gmail.com>
2019-07-24 16:24:42 -0400

    created the tables and ran the sql file against the database

---
+++ migrations/01_schema.sql
@@ -0,0 +1,48 @@
+-- drop table users;
+-- drop table properties;
+-- drop table reservations;
+-- drop table property_reviews;
+
+create table users (
+    id serial primary key not null,
+    name text not null,
+    email text not null,
+    password text not null
+);
+
+create table properties (
+    id serial primary key not null,
+    owner_id integer references users(id) on delete cascade,
+    title text not null,
+    description text not null,
+    thumbnail_photo_url text not null,
+    cover_photo_url text not null,
+    cost_per_night integer not null,
+    street text not null,
+    parking_spaces integer,
+    number_of_bathrooms integer,
+    number_of_bedrooms integer,
+    country text not null,
+    city text not null,
+    province text not null,
+    post_code text not null, 
+    active boolean not null
+ );
+
+create table reservations (
+    id serial primary key not null,
+    start_date date not null,
+    end_date date not null,
+    property_id integer references properties(id) on delete cascade,
+    guest_id integer references users(id) on delete cascade
+ );
+
+create table property_reviews (
+    id serial primary key not null,
+    guest_id integer references users(id) on delete cascade,
+    reservation_id integer references reservations(id) on delete cascade,
+    property_id integer references properties(id) on delete cascade,
+    rating integer,
+    message text
+);
+
